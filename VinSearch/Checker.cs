1f7a67d58ddf392f8729ed17119aff98d18901f0
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-25 15:05:14 -0500

    final

---
+++ VinSearch/Checker.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinSearch
+{
+    public class Checker
+    {
+        public Checker(Vin vinNumber)
+        {
+            VinNumber = vinNumber;
+        }
+
+        public Vin VinNumber { get; set; }
+
+        public bool IsValidDenati()
+        {
+            return
+                IsValidCountryCode() &&
+                IsValidManufacturerCode() &&
+                IsValidVehicleCode() &&
+                IsValidSecurityCode() &&
+                IsValidYearCode() &&
+                IsValidPlantCode();
+        }
+
+        public bool IsCoveredUnderWarranty()
+        {
+            return
+                IsValidCountryCode() &&
+                IsValidManufacturerCode() &&
+                IsValidWarrantyVehicleCode() &&
+                IsValidSecurityCode() &&
+                IsValidWarrantyYearCode() &&
+                IsValidPlantCode();
+        }
+
+        public bool IsValidCountryCode()
+        {
+            var problemYears = new List<string>()
+            {
+                "1", "2", "3", "4"
+            };
+
+            return problemYears.Contains(VinNumber.CountryCode);
+        }
+
+        public bool IsValidManufacturerCode()
+        {
+            return VinNumber.ManufacturerCode == "DN";
+        }
+
+        public bool IsValidVehicleCode()
+        {
+            return VinNumber.VehicleCode.Substring(4, 1) == "Z";
+        }
+
+        public bool IsValidWarrantyVehicleCode()
+        {
+            var problemVehicles = new List<string>()
+            {
+                "HE80Z", "JB70Z"
+            };
+
+            return problemVehicles.Contains(VinNumber.VehicleCode);
+        }
+
+        public bool IsValidSecurityCode()
+        {
+            return VinNumber.SecurityCode == "Z";
+        }
+
+        public bool IsValidYearCode()
+        {
+            var possibleYears = new List<String>()
+            {
+                "B", "C", "D", "E", "F"
+            };
+
+            return possibleYears.Contains(VinNumber.YearCode);
+        }
+
+        public bool IsValidWarrantyYearCode()
+        {
+            var problemYears = new List<String>()
+            {
+                "D", "E"
+            };
+
+            return problemYears.Contains(VinNumber.YearCode);
+        }
+
+        public bool IsValidPlantCode()
+        {
+            if (VinNumber.CountryCode == "1")
+                return VinNumber.PlantCode == "A" || VinNumber.PlantCode == "B";
+            if (VinNumber.CountryCode == "2")
+                return VinNumber.PlantCode == "A" || VinNumber.PlantCode == "B";
+            if (VinNumber.CountryCode == "3")
+                return VinNumber.PlantCode == "A" || VinNumber.PlantCode == "B";
+            if (VinNumber.CountryCode == "4")
+                return VinNumber.PlantCode == "A" || VinNumber.PlantCode == "B";
+
+            return false;
+        }
+    }
+}
--- VinSearch/Program.cs
+++ VinSearch/Program.cs
@@ -10,6 +10,27 @@ namespace VinSearch
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your Vin Number: ");
+            var vinNumber = Console.ReadLine();
+
+            var vin = new Vin(vinNumber);
+            var checker = new Checker(vin);
+
+            if (checker.IsCoveredUnderWarranty())
+            {
+                if (vin.VehicleCode == "HE80Z")
+                {
+                    Console.WriteLine("The Denati Company has had a recall on several cars and yours is one of them. It appears that you own the Hyper Elon Sport, and you are entitled to new underbody aluminum reference plates at a cost of €3000");
+                }
+                else if (vin.VehicleCode == "JB70Z")
+                {
+                    Console.WriteLine("The Denati Company has had a recall on several cars and yours is one of them. It appears that you own the James Bond Galore, and you are entitled to 4 new archanoid valves, each at a cost of €9.99.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately you are not covered under warranty");
+            }
         }
     }
 }
